5d5c639b14baf911d48efde27c68b8da1ac5d52d
Alan Lipschutz <alanelipschutz@gmail.com>
2021-06-28 16:40:58 -0300

    commit con html y css armado, falta subir img

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
+function deleteResults() {
+  const button = document.querySelector(".button-search-header");
+  const contentResultsEl = document.querySelector(".desafios");
+  button.addEventListener("click", () => (contentResultsEl.innerHTML = ""));
+}
+
 function showResults(results) {
   const contentResultsEl = document.querySelector(".desafios");
   const templateEl = document.querySelector("#template-content");
   for (const r of results) {
-    const titleEl = document.querySelector(".desafio-item-title");
+    const titleEl = templateEl.content.querySelector(".desafio-item-title");
     titleEl.textContent = r.fields.titulo;
-    // const imgEl = document.querySelector(".desafio-item-img");
-    // imgEl.src = r.field.
+    const descriptionEl = templateEl.content.querySelector(
+      ".desafio-item-description"
+    );
+    descriptionEl.textContent = r.fields.descripcion;
+    const linkEl = templateEl.content.querySelector(".desafio-item-link");
+    linkEl.href = r.fields.url;
+    const imgEl = templateEl.content.querySelector(".desafio-item-img");
 
     //   desafio-item-title, desafio-item-img, desafio-item-description, desafio-item-link
     //  que agarrar: titulo, descripcion, url,
@@ -16,12 +27,14 @@ function showResults(results) {
 
 function main() {
   const button = document.querySelector(".button-search-header");
+  deleteResults();
   button.addEventListener("click", () => {
     fetch(
       "https://cdn.contentful.com/spaces/prwodad3pg7p/environments/master/entries?access_token=gccrkxanjuuNEPsRAig5_FSTdZT20ps6ou4JopI7JL0"
     )
       .then((res) => res.json())
-      .then((data) => showResults(data.items));
+      .then((data) => console.log(data));
+    //   .then((data) => showResults(data.items));
   });
 }
 
